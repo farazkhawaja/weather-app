677dc2367f8ac9380c33d8c09a691f125863089f
cycnade <khwajafarazahmed@yahoo.com>
2020-05-06 22:39:46 +0500

    modifications

--- public/js/app.js
+++ public/js/app.js
@@ -12,7 +12,7 @@ document.querySelector("form").addEventListener("submit",function(e){
     fetch("/weather?address="+search).then(function(response){
     response.json().then(function(data){
         if (data.error){
-            document.querySelector(".loc").innerHTML="-"
+            document.querySelector(".loc").innerHTML="Try again"
             document.querySelector(".summ").innerHTML="-"
         } else{
         console.log(data.area)
@@ -26,7 +26,7 @@ document.querySelector("form").addEventListener("submit",function(e){
 })
 
 })
-document.querySelector("button").addEventListener("click",function(e){
+document.querySelector("#demo").addEventListener("click",function(e){
    e.preventDefault()
     fetch("/g").then(function(response){
         response.json().then(function(data){
